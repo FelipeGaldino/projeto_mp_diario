c3ccb8d2678adf8b1a9e0f5d33eaebdaf0d9672e
Andre <andremicael@gmail.com>
2025-10-14 14:56:08 -0400

    Initial commit: DiÃ¡rio Oficial Search System

---
+++ Downloads/Diário Oficial Search System/src/imports/Frame1.tsx
@@ -0,0 +1,73 @@
+function Group1() {
+  return (
+    <div className="absolute contents left-[515px] top-[832px]">
+      <div className="absolute bg-[#d9d9d9] h-[44px] left-[515px] rounded-[20px] top-[832px] w-[829px]" />
+      <div className="absolute bg-[#d9d9d9] h-[44px] left-[515px] rounded-[20px] top-[919px] w-[829px]" />
+      <div className="absolute bg-[#d9d9d9] h-[44px] left-[515px] rounded-[20px] top-[1006px] w-[829px]" />
+      <div className="absolute bg-[#d9d9d9] h-[44px] left-[515px] rounded-[20px] top-[1093px] w-[829px]" />
+      <div className="absolute bg-[#d9d9d9] h-[44px] left-[515px] rounded-[20px] top-[1180px] w-[829px]" />
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[583px] not-italic text-[25.662px] text-black text-nowrap top-[838px] whitespace-pre">Pergunta 1</p>
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[583px] not-italic text-[25.662px] text-black text-nowrap top-[925px] whitespace-pre">Pergunta 2</p>
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[583px] not-italic text-[25.662px] text-black text-nowrap top-[1012px] whitespace-pre">Pergunta 3</p>
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[583px] not-italic text-[25.662px] text-black text-nowrap top-[1099px] whitespace-pre">Pergunta 4</p>
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[583px] not-italic text-[25.662px] text-black text-nowrap top-[1186px] whitespace-pre">Pergunta 5</p>
+      <div className="absolute h-[11.5px] left-[1289px] top-[850px] w-[23px]" data-name="Vector">
+        <div className="absolute inset-[-16.67%_-8.33%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 27 16">
+            <path d="M2 2L13.5 13.5L25 2" id="Vector" stroke="var(--stroke-0, black)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3.83333" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute h-[11.5px] left-[1289px] top-[935px] w-[23px]" data-name="Vector">
+        <div className="absolute inset-[-16.67%_-8.33%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 27 16">
+            <path d="M2 2L13.5 13.5L25 2" id="Vector" stroke="var(--stroke-0, black)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3.83333" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute h-[11.5px] left-[1289px] top-[1020px] w-[23px]" data-name="Vector">
+        <div className="absolute inset-[-16.67%_-8.33%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 27 16">
+            <path d="M2 2L13.5 13.5L25 2" id="Vector" stroke="var(--stroke-0, black)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3.83333" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute h-[11.5px] left-[1289px] top-[1105px] w-[23px]" data-name="Vector">
+        <div className="absolute inset-[-16.67%_-8.33%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 27 16">
+            <path d="M2 2L13.5 13.5L25 2" id="Vector" stroke="var(--stroke-0, black)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3.83333" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute h-[11.5px] left-[1289px] top-[1190px] w-[23px]" data-name="Vector">
+        <div className="absolute inset-[-16.67%_-8.33%]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 27 16">
+            <path d="M2 2L13.5 13.5L25 2" id="Vector" stroke="var(--stroke-0, black)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3.83333" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function Frame1() {
+  return (
+    <div className="bg-white relative size-full">
+      <div className="absolute bg-[#d9d9d9] h-[97px] left-[171px] rounded-[51px] top-[526px] w-[1097px]" />
+      <div className="absolute bg-[#d9d9d9] h-[277px] left-0 top-[93px] w-[1440px]" />
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[131px] not-italic text-[41.519px] text-black text-nowrap top-[221px] whitespace-pre">Header</p>
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[230px] not-italic text-[37.833px] text-black text-nowrap top-[552px] whitespace-pre">Busca</p>
+      <div className="absolute bg-[#888888] h-[93px] left-0 top-0 w-[1440px]" />
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[131px] not-italic text-[41.519px] text-black text-nowrap top-[26px] whitespace-pre">Navbar</p>
+      <div className="absolute left-[1183px] size-[69px] top-[540px]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 69 69">
+          <circle cx="34.5" cy="34.5" fill="var(--fill-0, #5B5B5B)" fillOpacity="0.25" id="Ellipse 1" r="34.5" />
+        </svg>
+      </div>
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[131px] not-italic text-[41.519px] text-black text-nowrap top-[1003px] whitespace-pre">FAQ</p>
+      <div className="absolute bg-[#d9d9d9] h-[414px] left-0 top-[1446px] w-[1440px]" />
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[131px] not-italic text-[41.519px] text-black text-nowrap top-[1628px] whitespace-pre">Footer</p>
+      <Group1 />
+    </div>
+  );
+}
---
+++ Downloads/Diário Oficial Search System/src/imports/Frame3.tsx
@@ -0,0 +1,14 @@
+export default function Frame3() {
+  return (
+    <div className="bg-white relative size-full">
+      <div className="absolute bg-[#888888] h-[93px] left-0 top-0 w-[1440px]" />
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[137px] not-italic text-[32.366px] text-black text-nowrap top-[127px] whitespace-pre">Nome do usuário</p>
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[137px] not-italic text-[32.366px] text-black text-nowrap top-[168px] whitespace-pre">Orgao</p>
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[142px] not-italic text-[32.366px] text-black text-nowrap top-[262px] whitespace-pre">10 Menções encontradas</p>
+      <div className="absolute bg-[#d9d9d9] h-[193px] left-[142px] top-[350px] w-[1156px]" />
+      <div className="absolute bg-[#d9d9d9] h-[193px] left-[142px] top-[592px] w-[1156px]" />
+      <div className="absolute bg-[#d9d9d9] h-[193px] left-[142px] top-[834px] w-[1156px]" />
+      <p className="absolute font-['Inter:Regular',_sans-serif] font-normal leading-[normal] left-[89px] not-italic text-[41.519px] text-black text-nowrap top-[22px] whitespace-pre">Navbar</p>
+    </div>
+  );
+}
